06ba44376f4c9d922a6317763800b498d97b07a1
Esther Barthel <ebarthel@cognitionit.com>
2017-04-02 20:57:51 +0200

    NetScaler VPX Hypervisor Import Scripts
    Adding the NetScaler VPX Hypervisor Import scripts mentioned in the
    presentations

---
+++ HyperVisorImports/ESXImportNS_v1_1.ps1
@@ -0,0 +1,77 @@
+﻿<#
+.SYNOPSIS
+  Citrix NetScaler VPX import script for ESX.
+.DESCRIPTION
+  Citrix NetScaler VPX import script for ESX importing the virtual appliance with a fixed IP.
+.NOTES
+  Version:        1.2
+  Author:         Esther Barthel, MSc
+  Creation Date:  2016-01-16
+
+  Copyright (c) cognition IT. All rights reserved.
+
+  Requires a local installation of PowerCLI Snapins for PowerShell
+
+  Based upon Citrix Knowledgebase article: http://support.citrix.com/article/ctx128250
+#>
+
+# Constants
+$VMDefaultName = "NSVPX-ESX"                               # Default name of NetScaler VPX
+$FirstNICDefaultName = "Network adapter 1"                 # Default name of the first NIC
+ 
+# vSphere variables
+$vServer = "xxx.xxx.xxx.xxx"                               # IP address of the ESX host
+$vAppSource = "C:\Install\NSVPX-ESX-11.0-63.16_nc.ovf"     # NetScaler OVF file to import
+$ESXiHostName = "xxxxx"                                    # Name of the ESX(i) host
+ 
+# VM variables
+$VMName = "NSVPX-FixedIP"
+$IPaddress = "10.1.1.231"
+$SubnetMask = "255.255.255.0"
+$DefaultGW = "10.1.1.1"
+$VMNetworkName = "Internal Network"
+
+# Optional VM variable (can be useful for NetScaler license management). The MAC address must be in the valid "manual" range 00:50:56:00:00:00 - 00:50:56:3F:FF:FF.    
+$MACaddress = "00:50:56:34:05:58"
+ 
+#Load VMware PowerCLI Snapins
+If ((Get-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue) -eq $null)
+{
+    Add-PsSnapin VMware.VimAutomation.Core
+    Write-Verbose -Message "Adding the VMware.VimAutomation.Core PowerShell snap-in ..." -Verbose
+}
+ 
+# Connect to the vSphere server
+Connect-VIServer -Server $vServer -WarningAction SilentlyContinue
+ 
+# Import OVF on specified ESXi Host
+$vmHost = Get-VMHost -Name $ESXiHostName
+Import-vApp -Source $vAppSource -VMHost $vmHost -Force -WarningAction SilentlyContinue
+ 
+# Get VM (based upon default name after import)
+$VM = Get-VM -Name $VMDefaultName -Server $ESXiHostName
+ 
+# Change the Network Adapter network
+$VMFirstNIC = Get-NetworkAdapter -VM $VM | Where-Object {$_.Name -eq $FirstNICDefaultName}
+ 
+If (($MACaddress -eq $null) -or ($MACaddress -eq ""))
+{
+    # Change the first Network adapter's Network (MAC address is auto generated)
+    Set-NetworkAdapter $VMFirstNIC -NetworkName $VMNetworkName -Confirm:$false
+}
+Else
+{
+    # Change the first Network adapter's Network name and provide a fixed MAC address
+    Set-NetworkAdapter $VMFirstNIC -NetworkName $VMNetworkName -MacAddress $MACaddress -Confirm:$false
+}
+# Change the VM name
+Set-VM -VM $VM -Name $VMName -Confirm:$false
+ 
+# Configure fixed IP for NetScaler
+New-AdvancedSetting $VM -Type VM -Name "machine.id" -Value "ip=$IPaddress&netmask=$SubnetMask&gateway=$DefaultGW" -Confirm:$false -Force:$true -WarningAction SilentlyContinue
+ 
+# Start VM
+Start-VM $VM
+ 
+# Disconnect from the vShpere Server 
+Disconnect-VIServer -Server $vServer -Confirm:$false -WarningAction SilentlyContinue
---
+++ HyperVisorImports/XSImportNS_v1_1.ps1
@@ -0,0 +1,73 @@
+﻿<#
+.SYNOPSIS
+  Citrix NetScaler VPX import script for ESX.
+.DESCRIPTION
+  Citrix NetScaler VPX import script for ESX importing the virtual appliance with a fixed IP.
+.NOTES
+  Version:        1.1
+  Author:         Esther Barthel, MSc
+  Creation Date:  2015-11-15
+
+  Copyright (c) cognition IT. All rights reserved.
+
+  Requires a local installation of the XenServer PowerShell Module (SDK)
+
+  Based upon CTX article: http://support.citrix.com/article/CTX128236
+#>
+
+# Import XenServer 6.5.1 SDK
+Import-Module "C:\Install\XenServerPSModule"
+
+# Configure script variables
+$XSServer = "192.168.10.51"                                    # Hostname or IP-address of XenServer Poolmaster
+$sourcePath = "C:\Install\NSVPX-XEN-11.0-63.16_nc.xva"         # location of NS VPX image on machine running PowerShell
+$nsIPAddress = "192.168.10.6"                                  # fixed IP-address to configure for the NetScaler VPX
+$nsNetmask = "255.255.255.0"                                   # Netmask to configure for the NetScaler VPX
+$nsGateway = "192.168.10.1"                                    # Default Gateway to configure for the NetScaler VPX
+$XSHost = "HLXS01"                                             # The XenServer Host the VM is started on
+$VMName = "NSNitro"                                            # The new name of the VM
+$VMMACAddress = "1e:71:8a:76:f1:6e"                            # The new MAC Address to be given to the VM 1e718a76f16e
+$Networklabel = "Storage Network 10 (NIC1)"
+
+# Open a connection to XenServer (poolmaster) and make it the default session (required)
+$oXSSession = Connect-XenServer -Server $XSServer -SetDefaultSession
+
+# Import the NetScaler image to the default SR
+Import-XenVm -XenHost $XSServer -Path $sourcePath -Verbose
+
+# Get the imported NS VPX VM uuid by it's default name
+$oVM = Get-XenVM -Name "NetScaler Virtual Appliance"
+
+# Change the MAC address of the VM if a MAC address was specified
+If (!(($VMMACaddress -eq $null) -or ($VMMACaddress -eq "")))
+{
+    # Retrieve the current VIF object from the specified VM object
+    $oVIF = Get-XenVIF | Where-Object {$_.VM -eq $oVM}
+    # Retrieve the Network object from the specified Network label
+    $oNetwork = Get-XenNetwork -Name $Networklabel
+    # Remove the automatically assigned VIF
+    Remove-XenVIF -VIF $oVIF
+    # Create a new VIF for the give Network and VM objects with a specified MAC Address
+    New-XenVIF -VM $oVM -Network $oNetwork -MAC $VMMACAddress -Device "1"
+}
+
+# Change the name of the VM and make it start on the specified XS Host
+$oXS = Get-XenHost -Name $XSHost
+Set-XenVM -Uuid $oVM.uuid -NameLabel $VMName -Affinity $oXS
+
+# Get current VM XenStoreData values
+$newHash = $oVM.xenstore_data
+
+# Add required values for fixed IP settings for the NetScaler VPX
+$newHash.add("vm-data/ip",$nsIPAddress)
+$newHash.add("vm-data/netmask",$nsNetmask)
+$newHash.add("vm-data/gateway",$nsGateway)
+
+# Add new values to current VM XenStoreData (works only once, before NS is booted)
+Set-XenVM -VM $oVM -XenstoreData $newHash
+
+# Start the NS Appliance
+Invoke-XenVM $oVM -XenAction Start
+
+# Disconnect the session
+Get-XenSession -Server $XSServer | Disconnect-XenServer
